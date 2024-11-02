883d47ca479200c71f5be91387cc0a1d75e2fb82
kikevis <villasanchezg22@gmail.com>
2024-11-02 00:43:08 -0500

    config kike

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>
-          KikeViS
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React - KikeViS
-        </a>
-      </header>
+      Kike
     </div>
   );
 }
